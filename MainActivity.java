93f351930ec5bd091ef8ea699650ccdaa3ee24d2
Kostas <kotsaggelop@gmail.com>
2020-11-10 23:22:23 +0200

    Added MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.calculator;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    
+}
