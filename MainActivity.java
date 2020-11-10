6edf2adce33c331f77ef2734f043260f2fe457ad
michalisign <46412760+michalisign@users.noreply.github.com>
2020-11-10 23:26:43 +0200

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -8,5 +8,15 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    String oldNumber="";
+    String op = "+";
+    boolean isNewOp = true;
+    EditText ed1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ed1 = findViewById(R.id.editText);
+    }  
 }
