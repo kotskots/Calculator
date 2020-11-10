2021bd71112a949abcad67d7f0013e0e58c7deaa
mech-jpg <56562691+mech-jpg@users.noreply.github.com>
2020-11-10 23:32:10 +0200

    update numberEvent

--- MainActivity.java
+++ MainActivity.java
@@ -18,5 +18,46 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ed1 = findViewById(R.id.editText);
-    }  
+  } 
+    
+     public void numberEvent(View view) {
+        if(isNewOp)
+            ed1.setText("");
+            isNewOp = false;
+        String number = ed1.getText().toString();
+        switch (view.getId()){
+            case R.id.button_0:
+                number +=  "0";
+                break;
+            case R.id.button_1:
+                number +=  "1";
+                break;
+            case R.id.button_2:
+                number +=  "2";
+                break;
+            case R.id.button_3:
+                number +=  "3";
+                break;
+            case R.id.button_4:
+                number +=  "4";
+                break;
+            case R.id.button_5:
+                number +=  "5";
+                break;
+            case R.id.button_6:
+                number +=  "6";
+                break;
+            case R.id.button_7:
+                number +=  "7";
+                break;
+            case R.id.button_8:
+                number +=  "8";
+                break;
+            case R.id.button_9:
+                number +=  "9";
+                break;
+        }
+        ed1.setText(number);
+    }
 }
+
